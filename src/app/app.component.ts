9e149ebcd0b86c322efe86d71c759991119795bd
tmichielin <theodore.michielin@hotmail.fr>
2023-03-24 14:34:26 +0100

    ajout tableau

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,5 +12,32 @@ export class AppComponent {
 
   verif:boolean = false;
 
-  element:number=1
+  element:number=1;
+
+  etudiants:any[] = [
+    {"nom":"Dupont", "prenom":"Louis", "age":28},
+    {"nom":"Dupond", "prenom":"André", "age":24},
+    {"nom":"Lefebvre", "prenom":"Julie", "age":32},
+    {"nom":"Dumont", "prenom":"Marie-Louise", "age":23}
+  ]
+
+  choixClasse(isFirst:boolean, isLast:boolean, isEven:boolean)
+  {
+    var ma_classe:string="";
+    if(isFirst || isLast)
+    {
+      ma_classe = "first_last";
+    }
+    if(isEven)
+    {
+      ma_classe += "ligne_pair";
+    }
+    else
+    {
+      ma_classe += "ligne_impair"
+    }
+    return ma_classe;
+    
+  }
+
 }
